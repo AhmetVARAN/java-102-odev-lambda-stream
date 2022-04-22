975d6b1fa1ca976be0d9128161bc9c2925d25a5d
AhmetVARAN <102433124+AhmetVARAN@users.noreply.github.com>
2022-04-22 17:23:27 +0300

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,45 @@
+package HomeworkBook;
+
+public class Book {
+    String bookName;
+    int pageNumber;
+    String writerName;
+    String publishDate;
+
+    public Book(String bookName, int pageNumber, String writerName, String publishDate){
+        this.bookName=bookName;
+        this.pageNumber=pageNumber;
+        this.writerName=writerName;
+        this.publishDate=publishDate;
+    }
+    public String getBookName(){
+        return bookName;
+    }
+    public void setBookName(String bookName){
+        this.bookName=bookName;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public String getWriterName() {
+        return writerName;
+    }
+
+    public void setWriterName(String writerName) {
+        this.writerName = writerName;
+    }
+
+    public String getPublishDate() {
+        return publishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+}
